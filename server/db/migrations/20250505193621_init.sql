ccde0322047b934d120687e3e5ed952e9aa1ae26
Alexis Viscogliosi <alexis.viscogliosi@outlook.fr>
2025-05-08 10:13:45 +0200

    feat: update database schema to include layer digests and additional fields for images and layers

--- server/db/migrations/20250505193621_init.sql
+++ server/db/migrations/20250505193621_init.sql
@@ -1,7 +1,9 @@
+
 -- migrate:up transaction:false
 
 PRAGMA journal_mode = WAL;
 
+-- Base structure
 create table images
 (
     id           text primary key,
@@ -9,15 +11,19 @@ create table images
     identifier   text      not null,
     digest       text      not null,
     layers_count integer   not null,
+    layer_digests text     default '' not null,
     manifest     text      not null,
-    created_at   timestamp not null default CURRENT_TIMESTAMP
+    created_at   timestamp not null default CURRENT_TIMESTAMP,
+    used_at     timestamp not null default CURRENT_TIMESTAMP,
+    fs_index     BLOB      default null
 );
 
 create table layers
 (
     id         text primary key,
     digest     text      not null,
-    created_at timestamp not null default CURRENT_TIMESTAMP
+    created_at timestamp not null default CURRENT_TIMESTAMP,
+    fs_index   BLOB      default null
 );
 
 create table image_layers
@@ -25,16 +31,22 @@ create table image_layers
     image_id   text      not null,
     layer_id   text      not null,
     created_at timestamp not null default CURRENT_TIMESTAMP,
+    position   integer   default 0,
     primary key (image_id, layer_id),
     foreign key (image_id) references images (id),
     foreign key (layer_id) references layers (id)
 );
 
+-- Indexes
 create unique index images_digest_idx on images (digest);
 create unique index layers_digest_idx on layers (digest);
 
 -- migrate:down transaction:false
+
 PRAGMA journal_mode = WAL;
 
-drop table images;
+drop index images_digest_idx;
+drop index layers_digest_idx;
+drop table image_layers;
 drop table layers;
+drop table images;
